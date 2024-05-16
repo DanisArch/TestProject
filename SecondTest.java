bc1c04751592826073f3aab078494737c18eb18d
Mohammad Ali Rahmanian <158492651+RahmanianA@users.noreply.github.com>
2024-05-16 03:51:53 +0300

    new cod Edit

---
+++ SecondTest.java
@@ -0,0 +1,8 @@
+public class SecondTest {
+    public static void main(String[] args) {
+        String c= "cod ";
+        String b="Nevis";
+        String s= c + b;
+        System.out.println(s);
+    }
+}
